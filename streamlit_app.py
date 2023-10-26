0153b89b3603b8e2be3a20d1b33729273dc9813d
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2023-10-26 18:17:42 +0200

    Use abbreviation st for streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,16 @@
 import pandas as pd
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+st.header('Breakfast Menu')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
+st.text('🐔 Hard-boiled Free-Range Egg')
+st.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruit_url = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt' 
 my_fruit_list = pd.read_csv(fruit_url)
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
