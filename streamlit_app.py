5a254c821c057c28a922db7331672501570ad6a0
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2023-11-01 16:33:49 +0100

    Debug info

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,13 @@ import streamlit as st
 from snowflake import connector as sfc
 
 secrets = st.secrets['snowflake']
-my_cnx = sfc.connect(**secrets)
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text('Hello from Snowflake')
-st.text(my_data_row)
+st.text(secrets)
+# my_cnx = sfc.connect(**secrets)
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# st.text('Hello from Snowflake')
+# st.text(my_data_row)
 
     
 st.title('My Parents New Healthy Diner')
