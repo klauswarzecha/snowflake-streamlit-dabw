e71a87d04a86ec26fa95d0d22c2096a4321ae589
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2023-11-01 15:32:17 +0100

    Use get

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import requests
 import streamlit as st
-import snowflake.connector
+import snowflake.connector as sfc
 
-my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
+my_cnx = sfc.connect(st.secrets.get("snowflake"))
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
